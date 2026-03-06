d96330e15a95179a6c422549ee7fcff381c302d1
cyesson <chris.yesson@ioz.ac.uk>
2026-03-06 09:14:37 +0000

    Map using leaflet

--- BenthicImageProcessing/Example-LongEtAl-2020.R
+++ BenthicImageProcessing/Example-LongEtAl-2020.R
@@ -1,29 +1,19 @@
 # load map library
 require(terra)
+require(leaflet)
 
 # load station data
 d<-read.csv("https://raw.githubusercontent.com/cyesson/Training/master/BenthicImageProcessing/Stations-LongEtAl-2020.csv")
 
-# read example bathymetry grid
-r<-rast("https://github.com/cyesson/Training/raw/master/BenthicImageProcessing/Example-Bathymetry.tif")
-
 # convert points to a spatial grid
 d.sp<-vect(d, geom=c("Long", "Lat"), crs="+proj=longlat +datum=WGS84")
+d.sp
 
-# crop raster grid to around the points
-r.crop<-crop(r, ext(buffer(d.sp,5000)))
-
-# plot the grid 
-plot(r.crop, main="Coral density")
-
-# add station points - sized proportional to coral density
-points(d.sp, pch=16, col="darkred", cex=d$Cauliflower.Corals)
-
-# overlay station locations (show zeros)
-points(d.sp, pch="x", col="black")
-
-# add legend
-legend("topright", title="per (m2)", legend=1:3, pt.cex=1:3, pch=16, col="red")
+# plot points over satellite map, scale points to coral density
+leaflet(d.sp) %>% 
+  addProviderTiles(providers$Esri.WorldImagery, group="Satellite") %>%
+  addCircleMarkers(radius = ~(5*Cauliflower.Corals),
+                   label = ~paste("Station =",Station, ", N=", Cauliflower.Corals, sep=""))
 
 
 # scatter plot of depth vs density 
@@ -34,4 +24,3 @@ plot(d$Depth, d$Cauliflower.Corals, pch=16, cex=2, col="purple3",
 # simple smooth line showing changing relationship of depth & density 
 s<-smooth.spline(d$Depth, d$Cauliflower.Corals)
 lines(predict(s, min(d$Depth):max(d$Depth)), col="purple4", lwd=4)
-
